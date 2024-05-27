9011ebb18053e050f79af6f470fca5e8ba53fcbe
Johannes Kingma <j.kingma@sbw.edu>
2024-05-27 14:25:09 +0200

    function for getWehater

--- index.php
+++ index.php
@@ -2,16 +2,23 @@
 
 require 'inc/lib.php';
 
-$url     = "https://api.openweathermap.org/data/2.5/weather";
-$payload = [ 
-  'q' => SETTINGS['app']['City'],
-  'units' => 'metric',
-  'appid' => SETTINGS['app']['AppId']
-];
 
-$result = sendGet( $url, $payload, '' );
+$result = getWeather(SETTINGS['app']['city']);
 
 echo wrap_tag( 'p', 'Temp ' . $result['main']['temp'] );
 echo wrap_tag( 'p', 'Beschreibung ' . $result['weather'][0]['description'] );
 echo wrap_tag( 'p', 'Feuchtigkeit ' . $result['main']['humidity'] );
 echo wrap_tag( 'p', 'Wind ' . $result['wind']['speed'] );
+
+
+function getWeather( string $city )
+{
+
+  $url     = "https://api.openweathermap.org/data/2.5/weather";
+  $payload = [ 
+    'q' => $city,
+    'units' => 'metric',
+    'appid' => SETTINGS['app']['appId']
+  ];
+  return sendGet( $url, $payload, '' );
+}
